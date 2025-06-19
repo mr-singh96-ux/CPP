09ee82d9d0fef166127706154614df4c2ce6ca4b
Kiratbir Singh <khalsakiratbirsingh@gmail.com>
2025-06-19 19:17:26 +0530

    Initial Upload

---
+++ fav_number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter a number between 1 to 100: ";
+    cin>>a;
+    cout<<"Amazing!! That's my favorite number too!"<<endl;
+    cout<<"No really!!, "<<a<<" is my favorite number!"<<endl;
+    return 0;
+}
---
+++ in_user.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int user1;
+    cout<<"Enter any number between 1 to 100: ";
+    cin>>user1;
+    cout<<"You Entered:"<<user1<<endl;
+}
