6483819e53a6ce13eb143d64a6903f9554fb2dae
Kiratbir Singh <khalsakiratbirsingh@gmail.com>
2025-07-04 19:19:13 +0530

    new upload

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i;
+    cout<<"Enter number of terms: ";
+    cin>>n;
+    for(i=0;i<n;i++){
+        if(i%2==0){
+            cout<<i<<" ";
+        }}
+        cout<<endl;
+        for(i=0;i<n;i++){
+        if(i%2!=0){
+            cout<<i<<" ";
+        }}
+}
---
+++ vote_eli.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age>=18){
+        cout<<"Yes, u can vote.";
+    }
+    else{
+        cout<<"You can't able to vote.";
+    }
+}
